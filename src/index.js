1616a4511430460277d9d8a335ece2a5763265ac
rtxrulez <>
2020-02-20 20:56:07 +0300

    init: first variant program

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let arr = [];
+
+    if (!matrix) return arr;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 !== 0) {
+            // не четное
+            matrix[i].sort(function(a, b) {
+                return b - a; // в порядке убывания
+            });
+        } else {
+            matrix[i].sort(function(a, b) {
+                return a - b;
+            });
+        }
+        arr = [...arr, ...matrix[i]];
+    }
+
+    return arr;
+};
